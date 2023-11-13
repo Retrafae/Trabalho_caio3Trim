bbb4e047db4e14acdf2ff024883557ea906596ca
Retrafae <130366200+Retrafae@users.noreply.github.com>
2023-11-13 20:44:53 -0300

    Add files via upload

---
+++ Caio_Trabalho/javascript/script.js
@@ -0,0 +1,5 @@
+function showRecipeDetails(recipeName) {
+    // Implemente a lógica para exibir os detalhes da receita, redirecionar para uma página de detalhes, etc.
+    console.log('Mostrando detalhes da receita: ' + recipeName);
+   
+}
